873d613dd26bf44e5b9521d9e49f796483e1cbb7
MilomirRadojevic <milomir993@gmail.com>
2017-09-13 15:43:04 +0200

    Completed task 02

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
 let data = {};
+let cars = [];
+
+const createCarTable = function() {
+  let table = document.getElementById('carTable');
+  for (let i = 0; i < 2; i++) {
+    let row = document.createElement('tr'); 
+    for (let j = 0; j < 3; j++) {
+      let cell = document.createElement('td');
+      
+      let frame = document.createElement('div');
+      frame.className = 'frame';
+      frame.id = 'frame' + (3 * i + j);
+      
+      cell.appendChild(frame);
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+};
+
+const updateCarTable = function() {
+  let table = document.getElementById('carTable');
+  for(let i = 0; i < cars.length; i++) {
+    let img = document.createElement('img');
+    img.src = cars[i].image;
+    
+    let frame = document.getElementById('frame' + i);
+    frame.appendChild(img);
+  }
+};
 
 const loadJson = function() {
   let request = new XMLHttpRequest();
   request.open('GET', 'http://localhost/server.php/data');
   request.onload = function() {
     if (request.status === 200) {
-      data = request.response;
+      data = eval('(' + request.response + ')');
+      
+      cars = [];
+      data.cars.forEach((car) => {
+        cars.push(car);
+      });
+      updateCarTable();
     }
   };
   
   request.send();
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   loadJson();
+  createCarTable();
 });
